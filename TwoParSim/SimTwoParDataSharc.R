08e892e53c151a199809ae8785c91bf2ea17127b
Kevin Zhao <zhaok22@l5.nibi.sharcnet>
2026-08-24 21:56:55 -0400

    update twoparsim sharcnet sims

--- TwoParSim/SimTwoParDataSharc.R
+++ TwoParSim/SimTwoParDataSharc.R
@@ -12,33 +12,36 @@ getDoParWorkers()
 
 nR0 <- 300
 namp <- 300
-ninits <- 100
+nS0 <- 10
+nI0 <- 10
 maxper <- 10
 
 dyn.load(paste("SIRfun", .Platform$dynlib.ext, sep = ""))
+# REDO WITH I0 between 0.0002/10 to 0.0002*10
 
-for(chckpt in 0:9){
+for(chckpt in 3:9){
   bfd <-
-    foreach(i = seq(11/10 + chckpt*3, 4 + chckpt*3, length.out = nR0/10), .combine = "rbind") %:%
-    foreach(j = seq(0, 1, length.out = namp), .combine = "rbind") %:% 
-    foreach(k = seq(0.08, 0.12, length.out = ninits), .combine = "rbind") %dopar% {
+    foreach(i = seq(1.6 + chckpt*3, 4.5 + chckpt*3, length.out = nR0/10), .combine = "rbind") %:%
+    foreach(j = seq(1/300, 1, length.out = namp), .combine = "rbind") %:% 
+    foreach(k = seq(0.8/i, 1.2/i, length.out = nS0), .combine = "rbind") %:% 
+    foreach(l = seq(0.0002/10,  0.0002*10, length.out = nI0), .combine = "rbind") %dopar% {
       state <- c(S = k,
-                 I = 0.001)
-      parameters <- c(mu = 0.0231,
-                      gam = 24.37,
+                 I = l)
+      parameters <- c(mu = 0.02,
+                      gam = 24.33,
                       R0 = i,
                       a = j)
       
       rawbfddata <- as.data.frame(ode(y = state, times = times, func = "SIRmap", parms = parameters, dllname="SIRfun", initfunc = "initmod")) %>%
         filter(time %in% 950:1000) %>%
-        mutate(R0 = i, a = j, S0 = k)
+        mutate(R0 = i, a = j, S0 = k, I0 = l)
       bfddataper <- rawbfddata %>% tail(n=1)
       period <- length(unique(round(log(rawbfddata$I),4)))
       bfddataper$period <- ifelse(period > maxper, NA, period)
       return(bfddataper)
     }
-  saveRDS(bfd, file = paste("bfd_", chckpt+1, ".rds", sep = ""))
-  print(paste("bfd_", chckpt+1, ".rds created succesfully", sep = ""))
+  saveRDS(bfd, file = paste("updated_bfd_", chckpt+1, ".rds", sep = ""))
+  print(paste("updated_bfd_", chckpt+1, ".rds created succesfully", sep = ""))
 }
 ## Took 1m21s doing 30 * 30 * 10 sims and with 0.01 time steps with 10 cores,
 ## expected to take a little over 3 days with 30 cores and these parms.
--- TwoParSim/SimTwoParJob.sh
+++ TwoParSim/SimTwoParJob.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #SBATCH --account=def-earn
-#SBATCH --time=00:03:00
-#SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=256MB
-module load r/4.1.1
+#SBATCH --nodes=1
+#SBATCH --time=3-00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=4G
+#SBATCH --mail-user=zhaok22@mcmaster.ca
+#SBATCH --mail-type=ALL
+
+module load gcc/9.3.0 r/4.2.1
+export R_LIBS=~/local/R_libs/
 Rscript SimTwoParDataSharc.R
